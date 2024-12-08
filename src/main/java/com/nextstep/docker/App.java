a08123eea77ec56114666fd6386836ba05e5f60a
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2024-12-08 17:39:23 +0545

    making postgres image in docker

--- src/main/java/com/nextstep/docker/App.java
+++ src/main/java/com/nextstep/docker/App.java
@@ -3,6 +3,7 @@ package com.nextstep.docker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
