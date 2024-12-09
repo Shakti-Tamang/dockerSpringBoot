42fae68e50698022b25322dd34bd9270c739ac96
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2024-12-09 12:49:53 +0545

    making todoList systemstem

--- src/main/java/com/nextstep/docker/App.java
+++ src/main/java/com/nextstep/docker/App.java
@@ -1,10 +1,13 @@
 package com.nextstep.docker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "docker prac",version = "v1",description = "application docker compose"))
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
---
+++ src/main/java/com/nextstep/docker/Model/AuthUser.java
@@ -0,0 +1,17 @@
+package com.nextstep.docker.Model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "auth_user")
+public class AuthUser {
+
+
+
+}
