eb3ce83c270ef7feb98890c0213f7076a5f5d61c
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2024-12-12 15:01:25 +0545

    spring boot server admin

--- src/main/java/com/nextstep/docker/App.java
+++ src/main/java/com/nextstep/docker/App.java
@@ -1,5 +1,6 @@
 package com.nextstep.docker;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@EnableAdminServer
 @OpenAPIDefinition(info = @Info(title = "docker prac", version = "v1", description = "application docker compose"))
 public class App {
     public static void main(String[] args) {
